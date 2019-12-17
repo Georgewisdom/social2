85546b0f8a5450861deff121930275456c369b33
Georgewisdom <coppycatter100@gmail.com>
2019-12-17 14:52:00 +0100

    added test

---
+++ client/__tests__/App-test.js
@@ -0,0 +1,19 @@
+/**
+ * @format
+ */
+
+import 'react-native';
+import React from 'react';
+import App from '../App';
+
+
+// Note: test renderer must be required after react-native.
+import renderer from 'react-test-renderer';
+
+
+describe('App test', () => {
+  it('renders correctly', () => {
+      expect(1).toBe(1);
+  });
+})
+
